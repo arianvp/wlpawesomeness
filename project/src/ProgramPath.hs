7c1b93a1ee2cd268372271419784eb871edecef0
Arian van Putten <aeroboy94@gmail.com>
2016-12-05 12:12:52 +0100

    Start on program path generator

---
+++ project/src/ProgramPath.hs
@@ -0,0 +1,11 @@
+module ProgramPath where
+import Language
+
+type Depth = Int
+
+-- Gives all possible program paths of a program
+-- searching up to a given depth
+programPaths :: Program -> Depth -> [Statement]
+programPaths program depth = undefined
+
+
