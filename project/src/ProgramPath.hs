5805f1ca4f268c69ede58257023feb998ba3de07
Arian van Putten <aeroboy94@gmail.com>
2016-12-06 14:23:25 +0100

    Implement program path generator

--- project/src/ProgramPath.hs
+++ project/src/ProgramPath.hs
@@ -1,11 +1,17 @@
 module ProgramPath where
+
 import Language
+import Data.Monoid
+import Debug.Trace
 
-type Depth = Int
-
--- Gives all possible program paths of a program
--- searching up to a given depth
-programPaths :: Program -> Depth -> [Statement]
-programPaths program depth = undefined
-
+paths :: Int -> [Statement] -> [[Statement]]
+paths n [] = [[]]
+paths 0 xs = []
+paths n (If e s1 s2:xs) =
+  fmap (Assume e :) (paths (n - 1) (s1 <> xs)) <>
+  fmap (Assume (Not e) :) (paths (n - 1) (s2 <> xs))
+paths n (While e s:xs) =
+  fmap ((Assume e : s) <>) (paths (n - 1) (While e s : xs)) <>
+  fmap (Assume (Not e) :) (paths (n - 1) xs)
+paths n (e:xs) = fmap (e :) (paths n xs)
 
