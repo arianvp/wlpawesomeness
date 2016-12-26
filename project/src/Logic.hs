270bb78f70cbdd954e2ae2528706626ab5464df3
Arian van Putten <aeroboy94@gmail.com>
2016-12-26 14:30:06 +0100

    Stripping

--- project/src/Logic.hs
+++ project/src/Logic.hs
@@ -2,11 +2,14 @@
 module Logic where
 import Language
 
+-- strips away universal quantification at the beginning
+strip :: Expression -> Expression
+strip (Forall _ e) = strip e
+strip e = e
 
 normalize :: Expression -> Expression
-
 -- the main rewrite rule
-normalize (e1 :==>: (e2 :==>: e3)) =  normalize (normalize (e1 :&&: e2) :==>: normalize e3)
+normalize (e1 :==>: (e2 :==>: e3)) =  (normalize (e1 :&&: e2) :==>: normalize e3)
 
 -- flatten booleans
 normalize (BoolVal True :==>: e) = normalize e
@@ -35,7 +38,7 @@ normalize (e1 :-: e2) = normalize e1 :-: normalize e2
 normalize ((e1 :+: (IntVal x)) :+: (IntVal y)) = normalize e1 :+: IntVal (x+y)
 normalize (e1 :+: e2) = normalize e1 :+: normalize e2
 
--- normalize integer arithmatic
+
 
 normalize e = e
 
